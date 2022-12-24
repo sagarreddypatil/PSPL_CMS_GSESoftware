8bccf8b6589eacba082a2e8c2c706e3b86757054
Sagar Patil <sagarreddypatil@gmail.com>
2022-12-23 19:11:16 -0500

    some stuff

--- sensornet/ui/app.py
+++ sensornet/ui/app.py
@@ -1,21 +1,40 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import os
+
+bg_color = "#252526"
+text_color = "#FFFFFF"
+
+
+def abspath(filename: str):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+
+class Spacer(tk.Label):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.config(background=bg_color)
 
 
 class TextLabel(tk.Label):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.config(font=("Libre Franklin", 12))
+
+        self.config(font=("Libre Franklin", 12), background=bg_color, foreground=text_color)
 
 
 class HeaderLabel(tk.Label):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.config(font=("Saira Bold", 24))
+        self.config(font=("Saira Bold", 24), background=bg_color, foreground=text_color)
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        self.title("CMS SensorNet Decode")
+        self.configure(background=bg_color)
 
         for i in range(2):
             self.grid_columnconfigure(i, weight=1)
@@ -30,13 +49,14 @@ class App(tk.Tk):
             nonlocal row
             row += 1
 
-        # Create the title label
-        self.title_label = HeaderLabel(self, text="CMS SensorNet Decode")
-        self.title_label.grid(row=row, column=0, columnspan=2)
+        image = tk.PhotoImage(file=abspath("SensorNet Decode Header.png"))
+        image = image.subsample(2, 2)
+        self.title = tk.Button(self, text="bruh", image=image)
+        self.title.grid(row=row, column=0, columnspan=2)
         incrow()
 
         # add spacer
-        self.spacer = tk.Label(self, text="")
+        self.spacer = Spacer(self, text="")
         self.spacer.grid(row=row, column=0, columnspan=2)
         self.grid_rowconfigure(row, minsize=30)
         flexrow(1)
@@ -58,7 +78,7 @@ class App(tk.Tk):
         self.ip_display.grid(row=row, column=1, sticky="nsew")
         incrow()
 
-        self.spacer2 = tk.Label(self, text="")
+        self.spacer2 = Spacer(self, text="")
         self.spacer2.grid(row=row, column=0, columnspan=2)
         self.grid_rowconfigure(row, minsize=50)
         incrow()
