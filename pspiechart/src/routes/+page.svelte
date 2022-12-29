10edcee846741556f1a0e50068ebd12e3f06187a
Sagar Patil <sagarreddypatil@gmail.com>
2022-12-29 18:35:26 -0500

    some styles

--- pspiechart/src/routes/+page.svelte
+++ pspiechart/src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-	import Example from "./Example.svelte";
-
+	import Sidebar from './Sidebar.svelte'
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Example />
+<Sidebar />
---
+++ pspiechart/src/routes/Sidebar.svelte
@@ -0,0 +1,19 @@
+<script>
+	import pspColor from '$lib/assets/PSP-2Color.svg';
+</script>
+
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+	<a
+		href="/"
+		class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-black text-decoration-none"
+	>
+		<img src={pspColor} alt="logo" height="40px" />
+		<span class="fs-4">Mission Control</span>
+	</a>
+	<hr />
+	<ul class="nav nav-pills flex-column mb-auto">
+		<li class="nav-item">
+			<a href="#" class="nav-link active text-white" aria-current="page"> Home </a>
+		</li>
+	</ul>
+</div>
