74bbc8e61861822eeae63ad4449d943178e736b6
Sagar Patil <sagarreddypatil@gmail.com>
2022-12-29 03:40:22 -0500

    compose file that doens't work

--- configuration-ui/src/routes/query/+server.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const POST = (async () => {
-    return new Response(JSON.stringify([])); // Blank response; no plan to implement this
-}) satisfies RequestHandler;
