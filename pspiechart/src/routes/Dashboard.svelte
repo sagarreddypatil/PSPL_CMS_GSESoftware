f4f0c1123b4f7069b1c793b983b31f573fa89b6c
Sagar Patil <sagarreddypatil@gmail.com>
2023-01-01 04:47:46 -0500

    copy paste svelte-grid examples

--- pspiechart/src/routes/Dashboard.svelte
+++ pspiechart/src/routes/Dashboard.svelte
@@ -2,11 +2,15 @@
 	import Grid from 'svelte-grid';
 	import gridHelp from 'svelte-grid/build/helper/index.mjs';
 
+	const COLS = 6;
+
 	const id = () => '_' + Math.random().toString(36).substr(2, 9);
+
+	const randomNumberInRange = (min, max) => Math.random() * (max - min) + min;
 
 	let items = [
 		{
-			6: gridHelp.item({
+			[COLS]: gridHelp.item({
 				x: 0,
 				y: 0,
 				w: 2,
@@ -16,7 +20,7 @@
 		},
 
 		{
-			6: gridHelp.item({
+			[COLS]: gridHelp.item({
 				x: 2,
 				y: 0,
 				w: 2,
@@ -27,11 +31,67 @@
 	];
 
 	const cols = [[1100, 6]];
+
+	function add() {
+		let newItem = {
+			6: gridHelp.item({
+				w: Math.round(randomNumberInRange(1, 4)),
+				h: Math.round(randomNumberInRange(1, 4)),
+				x: 0,
+				y: 0
+			}),
+			id: id()
+		};
+
+		let findOutPosition = gridHelp.findSpace(newItem, items, COLS);
+
+		newItem = {
+			...newItem,
+			[COLS]: {
+				...newItem[COLS],
+				...findOutPosition
+			}
+		};
+
+		items = [...items, ...[newItem]];
+	}
+
+	const addAt = () => {
+		let newItem = {
+			6: gridHelp.item({
+				w: Math.round(randomNumberInRange(1, 4)),
+				h: Math.round(randomNumberInRange(1, 4)),
+				x: 0,
+				y: 0
+			}),
+			id: id()
+		};
+
+		items = [...[newItem], ...items];
+
+		items = gridHelp.adjust(items, COLS);
+	};
+
+	const remove = (item) => {
+		items = items.filter((value) => value.id !== item.id);
+		items = gridHelp.adjust(items, COLS);
+	};
 </script>
 
 <div class="flex-fill">
-	<Grid bind:items rowHeight={100} let:item let:dataItem {cols}>
-		<div class="dashboard-widget card bg-dark">{dataItem.id}</div>
+	<Grid {items} rowHeight={100} let:item {cols} let:dataItem>
+		<div class="dashboard-widget card bg-dark">
+			<span
+				on:pointerdown={(e) => e.stopPropagation()}
+				on:click={() => remove(dataItem)}
+				class="remove"
+			>
+				✕
+			</span>
+			<h3>
+				{dataItem.id}
+			</h3>
+		</div>
 	</Grid>
 </div>
 
