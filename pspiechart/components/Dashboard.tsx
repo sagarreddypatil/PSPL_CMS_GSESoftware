2846bde4537270822df7fc1733f6d8cc8f271d00
Sagar Patil <sagarreddypatil@gmail.com>
2023-01-02 13:44:47 -0500

    everything wrks

--- pspiechart/components/Dashboard.tsx
+++ pspiechart/components/Dashboard.tsx
@@ -1,55 +1,67 @@
-import styles from "../styles/Dashboard.module.scss";
-import "react-grid-layout/css/styles.css";
-import "react-resizable/css/styles.css";
 import { useEffect, useRef, useState } from "react";
 import ReactGridLayout from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
 import AutoSizer from "react-virtualized-auto-sizer";
+import { getSystemErrorMap } from "util";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function Grid() {
-  function genericCell(name) {
+  // create a state called layout
+  function genericCell(name: string) {
     return { i: name, x: 0, y: 0, w: 5, h: 5 };
   }
-
-  const layout = [
+  const [layout, setLayout] = useState([
     genericCell("a"),
     genericCell("b"),
     genericCell("c"),
     genericCell("d"),
     genericCell("e"),
-  ];
+  ]);
 
-  console.log(layout);
+  const genSmallLayout = (layout: ReactGridLayout.Layout[]) => {
+    if (layout.length === 0) return [];
+    let y = -layout[0].h;
+
+    return layout.map((item) => {
+      return {
+        ...item,
+        x: 0,
+        y: (y += item.h),
+      };
+    });
+  };
+
+  const layouts = {
+    lg: layout,
+    xss: genSmallLayout(layout),
+  };
+
+  console.log(layouts);
 
   return (
     <div
       className="flex-fill p-2"
       style={{ overflowY: "auto", overflowX: "hidden" }}
     >
-      <AutoSizer disableHeight>
-        {({ width }) => {
-          return (
-            <div style={{ width: `${width}px`, height: "100%" }}>
-              <ReactGridLayout
-                width={width}
-                isDraggable={true}
-                isResizable={true}
-                containerPadding={[0, 0]}
-                useCSSTransforms={false}
-                margin={[8, 8]}
-                cols={32}
-                rowHeight={32}
-                layout={layout}
-              >
-                {layout.map((item) => (
-                  <div className="card bg-dark" key={item.i}>
-                    {item.i}
-                  </div>
-                ))}
-              </ReactGridLayout>
-            </div>
-          );
-        }}
-      </AutoSizer>
+      <ResponsiveGridLayout
+        isDraggable={true}
+        isResizable={true}
+        containerPadding={[0, 0]}
+        useCSSTransforms={false}
+        margin={[8, 8]}
+        breakpoints={{ lg: 480, xxs: 0 }}
+        cols={{ lg: 25, xxs: 5 }}
+        rowHeight={32}
+        layouts={layouts}
+        onLayoutChange={(layout, layouts) => setLayout(layouts.lg)}
+      >
+        {layout.map((item) => (
+          <div className="card bg-dark" key={item.i}>
+            {item.i}
+          </div>
+        ))}
+      </ResponsiveGridLayout>
     </div>
   );
 }
