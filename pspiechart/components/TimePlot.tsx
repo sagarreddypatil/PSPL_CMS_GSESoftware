15ad8ec6d76809bd718b64b39c96eab1b7bb671c
Sagar Patil <sagarreddypatil@gmail.com>
2023-01-26 18:12:17 -0500

    fractal noise because

--- pspiechart/components/TimePlot.tsx
+++ pspiechart/components/TimePlot.tsx
@@ -1,6 +1,7 @@
 import { TimeDataPoint, TimeDataSource } from "@/types/DataInterfaces";
 import dynamic from "next/dynamic";
 import { useEffect, useRef, useState } from "react";
+import { DistributeVertical } from "react-bootstrap-icons";
 import uPlot from "uplot";
 
 const UPlotChart = dynamic(() => import("./UPlotChart"), {
@@ -10,6 +11,41 @@ const UPlotChart = dynamic(() => import("./UPlotChart"), {
 interface TimePlotProps {
   title: string;
   paused: boolean;
+}
+
+function psora(k: number, n: number) {
+  var r = Math.PI * (k ^ n);
+  return r - Math.floor(r);
+}
+
+function rng(x) {
+  return psora(x, 10) * 2 - 1;
+}
+
+function noise(x: number) {
+  const lo = Math.floor(x);
+  const hi = lo + 1;
+  const dist = x - lo;
+  const loSlope = rng(lo);
+  const hiSlope = rng(hi);
+  const loPos = loSlope * dist;
+  const hiPos = -hiSlope * (1 - dist);
+  const u = dist * dist * (3.0 - 2.0 * dist); // cubic curve
+  return loPos * (1 - u) + hiPos * u; // interpolate
+}
+
+function fractalNoise(x: number) {
+  const octaves = 10;
+  const persistence = 0.75;
+  let total = 0;
+
+  for (let i = 0; i < octaves; i++) {
+    const frequency = Math.pow(2, i);
+    const amplitude = Math.pow(persistence, i);
+    total += noise(x * frequency) * amplitude;
+  }
+
+  return total;
 }
 
 export default function TimePlot(props: TimePlotProps) {
@@ -26,8 +62,10 @@ export default function TimePlot(props: TimePlotProps) {
     return {
       time: time / 1000,
       // value: Math.sin((2 * 3.14159 * time) / 1000),
-      value: accRef.current,
-      // value: (time - start) / 1000,
+      // value: accRef.current,
+      value: fractalNoise(time / 1000),
+      // value: psora(time, 10),
+      // value: Math.random(),
     } as TimeDataPoint;
   };
 
