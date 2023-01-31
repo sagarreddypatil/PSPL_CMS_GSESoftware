b1be443872aeee6495415b6d9fd6d92ba270f354
Sagar Patil <sagarreddypatil@gmail.com>
2023-01-31 05:31:20 -0500

    build works

--- pspiechart/pages/dashboard/[dashboardId].tsx
+++ pspiechart/pages/dashboard/[dashboardId].tsx
@@ -1,25 +1,5 @@
-import Layout from "@/components/Layout";
-import Sidebar from "@/components/Sidebar";
 import Dashboard from "@/components/Dashboard/Dashboard";
 import { useRouter } from "next/router";
-import { getDashboards, DashboardStore } from "@/lib/dashboard-store";
-import Banner from "@/components/Hero";
-import { useContext, useEffect, useState } from "react";
-
-// let ws: WebSocket;
-
-// if (typeof window !== 'undefined') {
-//   // ws = new WebSocket(window.location.origin.replace(/^http/i, 'ws') + '/ws');
-//   // @ts-ignore
-//   // window.ws = ws;
-
-//   // 'open,message,close,error'.split(',').forEach(evt => {
-//   //   // @ts-ignore
-//   //   ws['on' + evt] = (...args: any[]) => {
-//   //     console.log('ws.on' + evt + ': ', args);
-//   //   };
-//   // })
-// }
 
 export default function Home() {
   const router = useRouter();
