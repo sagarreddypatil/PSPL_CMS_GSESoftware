a2c9c48eaa5f627b0b33ea207beefc5e5b366c6d
sagarreddypatil <sagarreddypatil@gmail.com>
2023-06-15 21:05:11 -0400

    fresh start

--- pspiechart/contexts/FullscreenContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const FullscreenContext = createContext<{
-  fullscreen: boolean;
-  setFullscreen: (fullscreen: boolean) => void;
-  // @ts-ignore
-}>(undefined);
--- pspiechart/contexts/WebsocketContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { WebsocketHandler } from "../handlers/WebsocketHandler";
-
-export const WebSocketContext = createContext<{
-  websocket: WebsocketHandler;
-  setWebSocket: (websocket: WebsocketHandler) => void;
-  // @ts-ignore
-}>(undefined);
