8eeaa694f771f84e211f21ee25160b831b01efd0
Sagar Patil <sagarreddypatil@gmail.com>
2024-01-05 15:32:08 -0500

    cmdnet v2 cli

---
+++ cmdnet-v2-cli/main.py
@@ -0,0 +1,61 @@
+import msgpack
+import socket
+import time
+
+
+host = "192.168.2.50"
+port = 8080
+timeout = 1
+
+
+def cmdnet_send(cmd):
+    start = time.time()
+    while True:
+        if time.time() - start > timeout:
+            raise Exception("Timeout")
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(timeout)
+            sock.connect((host, port))
+            sock.settimeout(None)
+            break
+        except:
+            continue
+
+    message = msgpack.packb(cmd)
+    sock.send(message)
+
+    recvd = b""
+    while True:
+        data = sock.recv(1024)
+        if not data:
+            break
+        recvd += data
+
+    print(recvd)
+
+    resp = msgpack.unpackb(recvd)
+    sock.close()
+
+    return resp
+
+
+if __name__ == "__main__":
+    while True:
+        raw_cmd = input("Command>")
+        raw_cmd = raw_cmd.split(" ")
+        # for each item, try to convert to int
+        cmd = []
+        for item in raw_cmd:
+            if not item:
+                continue
+            try:
+                cmd.append(int(item))
+            except:
+                cmd.append(item)
+
+        try:
+            resp = cmdnet_send(cmd)
+        except:
+            resp = "Timeout"
+        print(resp)
