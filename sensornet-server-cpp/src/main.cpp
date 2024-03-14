b193d241bfcdb383752baa5a6238425c90e19046
Sagar Patil <sagarreddypatil@gmail.com>
2024-03-14 02:25:06 -0400

    it worko

--- sensornet-server-cpp/src/main.cpp
+++ sensornet-server-cpp/src/main.cpp
@@ -4,55 +4,142 @@
 
 #include <nlohmann/json.hpp>
 #include <database.hpp>
-#include "App.h"
+#include <exprtk.hpp>
+#include <boost/serialization/map.hpp>
+#include <App.h> // uWebSockets, not sure why it's not nested
+
+#include "SensorModel.hpp"
 
 using json = nlohmann::json;
 static const uint port = 3180;
+static const uint udpPort = 3746;
 
 struct PerSocketData {
     /* Fill with user data */
 };
 
-struct SensorModel {
-    std::string id;
-    std::string name;
-    std::string unit;
-    std::string calibration;
+uWS::App* papp = nullptr;
+tsdb::Database* ptimedb = nullptr;
+SensorsStore* psdb = nullptr;
+
+struct SensorNetRemotePacket {
+    // 2 bytes id, 6 bytes pad, 8 byte timestamp, counter, value, value is signed
+    uint16_t id;
+    uint8_t pad[6];
+    uint64_t timestamp;
+    uint64_t counter;
+    int64_t value;
+} __attribute__((packed));
+
+struct SensorNetStored {
+    uint64_t counter;
+    int64_t value;
 };
 
-class JSONDb {
-    std::string loc;
-    json db;
+double apply_calibration(const std::string& sensor_id, const int64_t value) {
+    auto& sensor = psdb->get(sensor_id);
 
-public:
-    JSONDb(std::string loc, json init) : loc(loc) {
-        std::ifstream file(loc);
+    double dvalue = (double) value;
 
-        if (file.is_open()) {
-            file >> db;
-            file.close();
+    exprtk::symbol_table<double> symbol_table;
+
+    symbol_table.add_variable("x", dvalue, true);
+    exprtk::expression<double> expression;
+    expression.register_symbol_table(symbol_table);
+
+    exprtk::parser<double> parser;
+    parser.compile(sensor.calibration, expression);
+
+    return expression.value();
+}
+
+void handle_packet(char* payload, int length) {
+    if (length % sizeof(SensorNetRemotePacket) != 0) {
+        // TODO: remove error log, might be slow
+        std::cerr << "Invalid packet length" << std::endl;
+        return;
+    }
+
+    int packets = length / sizeof(SensorNetRemotePacket);
+    SensorNetRemotePacket* packet = (SensorNetRemotePacket*) payload;
+
+    for (int i = 0; i < packets; i++) {
+        SensorNetRemotePacket* p = packet + i;
+
+        if(!psdb->exists(std::to_string(p->id))) {
+            // sensor not found
+            return;
         }
-        else {
-            db = init;
-            save();
+
+        auto table = ptimedb->get_table<SensorNetStored>(std::to_string(p->id));
+        table->append(p->timestamp, {p->counter, p->value});
+
+        // send it over websocket
+        double value_calibrated = apply_calibration(std::to_string(p->id), p->value);
+
+        json wsPacket = {
+            {"id", std::to_string(p->id)},
+            {"timestamp", (double)p->timestamp},
+            {"value", value_calibrated}
+        };
+
+        papp->publish(std::to_string(p->id), wsPacket.dump(), uWS::OpCode::TEXT, false);
+    }
+}
+
+void on_server_drain(us_udp_socket_t *s) {}
+
+void on_server_data(us_udp_socket_t *s, us_udp_packet_buffer_t *buf, int packets) {
+    for (int i = 0; i < packets; i++) {
+        char *payload = us_udp_packet_buffer_payload(buf, i);
+        int length = us_udp_packet_buffer_payload_length(buf, i);
+
+        handle_packet(payload, length);
+    }
+}
+
+template<bool SSL>
+void allow_cors(uWS::HttpResponse<SSL> *res) {
+    res->writeHeader("Access-Control-Allow-Origin", "*");
+    res->writeHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res->writeHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
+std::map<std::string, std::string> parseQueryString(const std::string& queryString) {
+    std::map<std::string, std::string> result;
+
+    // Split the query string by '&'
+    size_t pos = 0;
+    while (pos < queryString.size()) {
+        size_t found = queryString.find('&', pos);
+        if (found == std::string::npos) {
+            found = queryString.size();
         }
+        std::string pair = queryString.substr(pos, found - pos);
+
+        // Split each pair by '='
+        size_t sepPos = pair.find('=');
+        if (sepPos != std::string::npos) {
+            std::string key = pair.substr(0, sepPos);
+            std::string value = pair.substr(sepPos + 1);
+            result[key] = value;
+        }
+
+        pos = found + 1; // Move to the next pair
     }
 
-    void save() {
-        std::ofstream file(loc);
-        file << db.dump(4);
-        file.close();
-    }
-
-    json &get() {
-        return db;
-    }
-
-};
+    return result;
+}
 
 int main() {
     uWS::App app = uWS::App();
-    JSONDb db("sensor_list.json", json::array());
+    papp = &app;
+
+    tsdb::Database timedb("tsdb");
+    ptimedb = &timedb;
+
+    SensorsStore sdb("sensors.dat");
+    psdb = &sdb;
 
     app.ws<PerSocketData>("/data", {
         .message = [](auto *ws, std::string_view message, uWS::OpCode opCode) {
@@ -65,32 +152,41 @@ int main() {
     });
 
     app.get("/", [](auto *res, auto *req) {
+        allow_cors(res);
         res->end("Hello world!");
     });
 
+    app.options("/*", [](auto *res, auto *req) {
+        allow_cors(res);
+        res->end();
+    });
+
     app.get("/sensors", [&](auto *res, auto *req) {
-        res->end(db.get().dump());
+        allow_cors(res);
+        json sensors = sdb.all();
+        res->end(sensors.dump());
     });
 
     app.get("/sensors/:id", [&](auto *res, auto *req) {
+        allow_cors(res);
         std::string id(req->getParameter(0));
-        json &sensors = db.get();
 
-        for (auto &sensor : sensors) {
-            if (sensor["id"] == id) {
-                res->end(sensor.dump());
-                return;
-            }
+        if(!sdb.exists(id)) {
+            res->writeStatus("404 Not Found")
+            ->end("Sensor not found");
         }
-    
-        res->writeStatus("404 Not Found");
-        res->end("Sensor not found");
+        else {
+            auto& sensor = sdb.get(id);
+            json sensorJson = sensor;
+            res->end(sensorJson.dump());
+        }
     });
 
     app.post("/sensors", [&](auto *res, auto *req) {
+        allow_cors(res);
         std::string buffer;
 
-        res->onData([res, buffer = std::move(buffer), &db](std::string_view data, bool last) mutable {
+        res->onData([res, buffer = std::move(buffer), &sdb](std::string_view data, bool last) mutable {
             buffer.append(data.data(), data.length());
 
             if (last) {
@@ -98,23 +194,29 @@ int main() {
 
                 // verify
                 if (!sensor.contains("id") || !sensor.contains("name") || !sensor.contains("unit") || !sensor.contains("calibration")) {
-                    res->writeStatus("400 Bad Request");
-                    res->end("Invalid sensor data");
+                    res->writeStatus("400 Bad Request")
+                    ->end("Invalid sensor data");
                     return;
                 }
 
-                db.get().push_back(sensor);
-                db.save();
-
-                res->end("Sensor added");
+                sdb.add(sensor.get<SensorModel>());
+                sdb.sync();
             }
         });
+
+        res->onAborted([res] {
+            res->writeStatus("400 Bad Request")
+            ->end("Aborted");
+        });
+
+        res->end("Sensor added");
     });
 
     app.post("/sensors/bulk", [&](auto *res, auto *req) {
+        allow_cors(res);
         std::string buffer;
 
-        res->onData([res, buffer = std::move(buffer), &db](std::string_view data, bool last) mutable {
+        res->onData([res, buffer = std::move(buffer), &sdb](std::string_view data, bool last) mutable {
             buffer.append(data.data(), data.length());
 
             if (last) {
@@ -134,65 +236,98 @@ int main() {
                     }
                 }
 
-                db.get().insert(db.get().end(), sensors.begin(), sensors.end());
-                db.save();
-
-                res->end("Sensors added");
+                sdb.update(sensors.get<std::vector<SensorModel>>());
+                sdb.sync();
             }
         });
+
+        res->onAborted([res] {
+            res->writeStatus("400 Bad Request")
+            ->end("Aborted");
+        });
+
+        res->end("Sensor added");
     });
 
     app.put("/sensors/:id", [&](auto *res, auto *req) {
+        allow_cors(res);
         std::string id(req->getParameter(0));
         std::string buffer;
 
-        res->onData([res, buffer = std::move(buffer), &db, id](std::string_view data, bool last) mutable {
+        res->onData([res, buffer = std::move(buffer), id, &sdb](std::string_view data, bool last) mutable {
             buffer.append(data.data(), data.length());
 
             if (last) {
                 json sensor = json::parse(data);
-                json &sensors = db.get();
 
-                for (auto &s : sensors) {
-                    if (s["id"] == id) {
-                        s = sensor;
-                        db.save();
-                        res->end();
-                        return;
-                    }
-                }
+                sdb.update(sensor.get<SensorModel>());
 
                 res->writeStatus("404 Not Found");
                 res->end("Sensor not found");
             }
         });
+
+        res->onAborted([res] {
+            res->writeStatus("400 Bad Request")
+            ->end("Aborted");
+        });
+
+        res->end("Sensor updated");
     });
 
     app.del("/sensors/:id", [&](auto *res, auto *req) {
+        allow_cors(res);
         std::string id(req->getParameter(0));
-        json &sensors = db.get();
 
-        for (auto it = sensors.begin(); it != sensors.end(); it++) {
-            if ((*it)["id"] == id) {
-                sensors.erase(it);
-                db.save();
-                res->end();
-                return;
-            }
-        }
+        sdb.remove(id);
 
         res->writeStatus("404 Not Found");
         res->end("Sensor not found");
     });
 
-    app.get("/sources", [](auto *res, auto *req) {
-        res->end("Sources");
+    app.get("/sources", [&](auto *res, auto *req) {
+        allow_cors(res);
+        // same thing as sensors
+        json sensors = sdb.all();
+        res->end(sensors.dump());
     });
 
     app.get("/historical/:id", [](auto *res, auto *req) {
+        allow_cors(res);
         std::string id(req->getParameter(0));
 
-        res->end("Historical data for " + id);
+        auto query = std::string(req->getQuery());
+        auto params = parseQueryString(query);
+
+        double start, end, dt;
+
+        try {
+            start = std::stod(params["start"]);
+            end = std::stod(params["end"]);
+            dt = std::stod(params["dt"]);
+        }
+        catch (std::invalid_argument& e) {
+            res->writeStatus("400 Bad Request")
+            ->end("Invalid query parameters");
+            return;
+        }
+
+        uint64_t start_us = start * 1e6;
+        uint64_t end_us = end * 1e6;
+        uint64_t dt_us = dt * 1e6;
+
+        auto table = ptimedb->get_table<SensorNetStored>(id);
+        auto data = table->reduce(start_us, end_us, dt_us);
+
+        json resp = json::array();
+        for (auto& entry : data) {
+            resp.push_back({
+                {"timestamp", (double)entry.timestamp}, // convert to ms
+                {"value", apply_calibration(id, entry.value.value)}
+            });
+        }
+
+        res->end(resp.dump());
     });
 
     app.listen(port, [](auto *listen_socket) {
@@ -203,6 +338,18 @@ int main() {
             std::cout << "Failed to listen on port " << port << std::endl;
         }
     });
+
+    us_loop_t* loop = (us_loop_t*) uWS::Loop::get();
+    us_udp_packet_buffer_t *receive_buf = us_create_udp_packet_buffer();
+    us_udp_socket_t *server = us_create_udp_socket(loop, receive_buf, on_server_data, on_server_drain, "0.0.0.0", udpPort, 0);
+
+    if(!server) {
+        std::cerr << "Failed to create UDP server" << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << "UDP server created" << std::endl;
+    }
 
     app.run();
 
---
+++ sensornet-server-cpp/test.py
@@ -0,0 +1,77 @@
+import socket
+import struct
+import random
+import math
+import time
+from random import gauss as randnorm
+
+
+"""
+Packet Format
+    H: Sensor ID (u16)
+    2x: Padding bytes (4-byte aligned)
+    Q: Timestamp (u64 microseconds)
+    Q: Counter (u64)
+    q: Data (i64)
+"""
+packet_format = "<H 6x Q Q q"
+fmt_compiled = struct.Struct(packet_format)
+
+
+def make_packet(
+    header: bytes, sensor_id: int, timestamp: int, counter: int, data: int
+) -> bytes:
+    return fmt_compiled.pack(sensor_id, timestamp, counter, data)
+
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Server IP address and port
+server_ip = "127.0.0.1"
+server_port = 3746
+
+ids = list(range(1, 6))
+counters = [0 for a in range(ids[-1] + 1)]
+datas = [0 for a in range(ids[-1] + 1)]
+
+rate = 1000  # Hz
+
+# start = time.time() # to test what would happen if time started at zero
+start = 0
+
+
+def better_sleep(sleep_time):
+    time.sleep(sleep_time)
+    # start = time.time()
+    # while time.time() - start < sleep_time:
+    #     pass
+
+
+last = time.time()
+while True:
+    for sensor_id in ids:
+        if sensor_id == 0:
+            continue
+        counter = counters[sensor_id]
+        counter = counters[sensor_id] = counter + 1
+
+        datas[sensor_id] += randnorm(0, 1) * 100 / rate
+        # data = datas[sensor_id] + math.sin(time.time() * 3) * 1000
+        data = datas[sensor_id] * 10
+        # datas[sensor_id] = data = math.sin(time.time() * 3 + sensor_id * math.pi / 2) * 1000
+
+        timestamp = int((time.time() - start) * 1000 * 1000)  # microseconds
+        data = int(data)
+        packet = make_packet(b"SEN", sensor_id, timestamp, counter, data)
+        # time.sleep(1)
+
+        sock.sendto(packet, (server_ip, server_port))
+
+    diff = time.time() - last
+    sleepval = (1 / rate) - diff
+    if sleepval > 0:
+        better_sleep(sleepval)
+    diff = time.time() - last
+    # print(f"Rate: {1 / (max(0.00001, diff))} Hz")
+    last = time.time()
